a7701c24f46afe3ed160ba59a99ef47ee209227c
olivierdourdin148@gmail.com <116560924+OLIDRN@users.noreply.github.com>
2023-05-15 12:58:35 +0200

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module spaceinvader.spaceinvader {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+                    requires org.kordamp.bootstrapfx.core;
+                requires com.almasb.fxgl.all;
+    
+    opens spaceinvader.spaceinvader to javafx.fxml;
+    exports spaceinvader.spaceinvader;
+}
---
+++ src/main/java/spaceinvader/spaceinvader/HelloController.java
@@ -0,0 +1,14 @@
+package spaceinvader.spaceinvader;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
